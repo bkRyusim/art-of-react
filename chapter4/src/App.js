f3ea8d8f04f9e5c9262273395925f4560cb0ac8d
SeungYunBaek <ryusim@cau.ac.kr>
2022-09-30 15:27:12 +0900

    [chapter4] Study about event handler

---
+++ chapter4/src/App.js
@@ -0,0 +1,14 @@
+import EventPractice from "./EventPractice";
+import FunctionPractice from "./FunctionPractice";
+
+function App() {
+  return (
+    <>
+      <EventPractice/>
+      <FunctionPractice/>
+    </>
+
+  );
+}
+
+export default App;
