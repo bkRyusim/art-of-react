7c066d4d0dac6b5c740112753a37cfeb56cf94bc
SeungYunBaek <ryusim@cau.ac.kr>
2022-10-02 03:10:06 +0900

    [chapter8] Study about Hooks

---
+++ chapter8/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Counter from "./Counter";
+import Info from "./Info";
+import ReducerInfo from "./ReducerInfo";
+import Average from "./Average";
+import CustomInfo from "./CustomInfo";
+
+function App() {
+  return (
+    <>
+      <Counter/>
+      <br/>
+      <Info/>
+      <br/>
+      <ReducerInfo/>
+      <Average/>
+      <CustomInfo/>
+    </>
+  );
+}
+
+export default App;
